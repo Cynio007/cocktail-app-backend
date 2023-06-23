99c08e6206b2d1d557e9be54206106cfc6c18501
Cynio007 <btp22@wp.pl>
2023-06-23 20:24:35 +0200

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', //dodaje możliwośc łączenia z backendem z innej domeny. Zmienić na właściwy, gdy nie używamy lokalnie na swoim PC
+  // });
   app.enableCors({
-    origin: 'http://localhost:3000', //dodaje możliwośc łączenia z backendem z innej domeny. Zmienić na właściwy, gdy nie używamy lokalnie na swoim PC
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    origin: 'http://localhost:3000',
   });
   app.use(cookieParser());
   await app.listen(3001);
